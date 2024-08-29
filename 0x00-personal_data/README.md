a9a32d0f306a12f5fc8cb20a88badb7ab41c1280
Benjamin Torfu <btorfu@proton.me>
2024-08-29 23:42:24 +0000

    obfuscated

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,3 @@
+Personal data
+
+Personally identifiable information (PII) and personal data are two classifications of data that often confuse organizations that collect, store and analyze such data. Both terms cover common ground, classifying information that could reveal an individual’s identity directly or indirectly. 
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Module containing filtered_logger function
+"""
+
+import re
+
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """
+        Returns the log message obfuscated
+    """
+    pattern = f'({"|".join([f"{field}=[^{separator}]*" for field in fields])})'
+    return re.sub(pattern, lambda m: f'{m.group(0).split("=")[0]}={redaction}', message)
