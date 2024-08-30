3082aeaa0ed9ae52d6ebab00ddbfe9249f4abe4c
Benjamin Torfu <btorfu@proton.me>
2024-08-30 00:00:25 +0000

    obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
     Module containing filtered_logger function
 """
 
+import logging
 import re
 
 
@@ -10,8 +11,26 @@ def filter_datum(fields: list,
                  redaction: str, message: str, separator: str) -> str:
     """
         Returns the log message obfuscated
-    """
+
     pattern = f'({"|".join([f"{field}=[^{separator}]*"
                             for field in fields])})'
     return re.sub(pattern,
                   lambda m: f'{m.group(0).split("=")[0]}={redaction}', message)
+    """
+    return (re.sub(f'({"|".join(fields)})=[^{separator}]*', lambda m:
+                   f'{m.group(1)}={redaction}', message))
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
