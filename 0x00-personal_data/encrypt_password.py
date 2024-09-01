222a7f76e7985938ba70dc7c71fa28367bd52953
Benjamin Torfu <btorfu@proton.me>
2024-09-01 01:40:06 +0000

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,11 +5,13 @@ Personal data
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """ Hash password
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Validate password"""
