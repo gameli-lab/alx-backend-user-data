2cdef5425846b03343de5010841112d1bf91b053
Benjamin Torfu <btorfu@proton.me>
2024-09-03 17:04:58 +0000

    user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,3 +59,24 @@ class BasicAuth(Auth):
 
         credentials = decoded_base64_authorization_header.split(':', 1)
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+        User object from credentials
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        
