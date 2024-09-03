1535b74340f0b1bc0337f2b9c891d27225d440d8
Benjamin Torfu <btorfu@proton.me>
2024-09-03 14:54:25 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,15 @@ class BasicAuth(Auth):
     Basic Auth
     """
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extract base64
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header,str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header[6:]
