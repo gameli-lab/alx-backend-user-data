bff41ff449f938c323ae4277f8e452a58254abb9
Benjamin Torfu <btorfu@proton.me>
2024-09-04 13:26:22 +0000

    fixed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -78,7 +78,7 @@ class BasicAuth(Auth):
             return None
 
         users = User.search({'email': user_email})
-        if not users:
+        if not users or len(users) == 0:
             return None
 
         user = users[0]
