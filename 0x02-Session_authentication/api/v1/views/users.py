337e6a7df8e59c1e3510891df14c67507e85bfb7
Benjamin Torfu <btorfu@proton.me>
2024-09-07 17:53:40 +0000

    me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -29,7 +29,7 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())
-    if user_id is None:
+    if not user_id:
         abort(404)
     user = User.get(user_id)
     if user is None:
