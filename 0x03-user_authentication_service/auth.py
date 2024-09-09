23134ae6490f4e6a83d9f9a6ccd32fab4d816f8c
Benjamin Torfu <btorfu@proton.me>
2024-09-09 19:12:39 +0000

    user, auth & db

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""Auth module"""
+
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """initiates the class"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            self._db.add_user(email=email,
+                              hashed_password=hashed_password)
+            return self._db.find_user_by(email=email)
